05e85131fc71d5806ef3de68e9164b61690b7edd
Musa Mutetwi <musasoftlabx@gmail.com>
2025-10-20 19:56:24 +0300

    Testing

--- src/__tests__/Revenue.test.tsx
+++ src/__tests__/Revenue.test.tsx
@@ -3,12 +3,19 @@ import userEvent from "@testing-library/user-event";
 import Revenue from "@/app/revenue/page";
 import App from "@/app/layout";
 
-//test("renders correctly", async () => render(<App children={<div />} />));
 test("renders correctly", async () => render(<Revenue />));
 
-// test("transactions pulled from API", async () => {
-//   render(<Revenue />);
+test("transactions pulled from API", async () => {
+  const transactions = await screen.findAllByRole("listitem");
+  expect(transactions).toHaveLength(7);
+});
 
-//   const transactions = await screen.findAllByRole("listitem");
-//   expect(transactions).toHaveLength(3);
-// });
+test("filter form", async () => {
+  const filters = await screen.findAllByRole("menu");
+  expect(filters).toBeVisible();
+});
+
+test("filter form", async () => {
+  const filters = await screen.findAllByRole("menu");
+  expect(filters).toBeVisible();
+});

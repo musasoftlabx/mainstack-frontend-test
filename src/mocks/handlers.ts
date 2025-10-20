5e27ffb7624de2ce1daceffb544104826505478a
Musa Mutetwi <musasoftlabx@gmail.com>
2025-10-20 19:58:47 +0300

    Testing

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,10 +1,10 @@
 import { http, HttpRequestHandler } from "msw";
 
 export const handlers = [
-  http.get(
-    `${process.env.NEXT_PUBLIC_API}transactions`,
-    (req: HttpRequestHandler, res: HttpRequestHandler, ctx: any) => {
-      return res(ctx.status(200), ctx.json([{ amount: 500 }]));
-    }
-  ),
+  // http.get(
+  //   `${process.env.NEXT_PUBLIC_API}transactions`,
+  //   (req: HttpRequestHandler, res: HttpRequestHandler, ctx: any) => {
+  //     return res(ctx.status(200), ctx.json([{ amount: 500 }]));
+  //   }
+  // ),
 ];
